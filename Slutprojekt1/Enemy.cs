3c2d24fb2b34b33c19684bfcb3689a5f496fd916
Nabil Naqos <zidane.nabil07@gmail.com>
2026-03-04 23:19:26 +0100

    3

---
+++ Slutprojekt1/Enemy.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Slutprojekt1;
+
+public class Enemy : Fighter
+{
+public int difficulty { get; private set; }
+
+}
--- Slutprojekt1/Fighter.cs
+++ Slutprojekt1/Fighter.cs
@@ -4,13 +4,18 @@ namespace Slutprojekt1;
 
 public class Fighter
 {
-    public int Hp { get; set; }
+     
+    public int Health { get; set; }
+
+    public int MaxHealth { get; set; }
 
     public int Attack { get; set; }
 
     public int Defense { get; set; }
 
     public int Stamina { get; set; }
+
+    
 
 }
 
