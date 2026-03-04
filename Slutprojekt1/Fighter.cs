f83ab2beed411241bd8361a807fc23b5dca814ea
Nabil Naqos <zidane.nabil07@gmail.com>
2026-03-04 13:31:47 +0100

    2

--- Slutprojekt1/Fighter.cs
+++ Slutprojekt1/Fighter.cs
@@ -4,5 +4,13 @@ namespace Slutprojekt1;
 
 public class Fighter
 {
+    public int Hp { get; set; }
+
+    public int Attack { get; set; }
+
+    public int Defense { get; set; }
+
+    public int Stamina { get; set; }
 
 }
+
