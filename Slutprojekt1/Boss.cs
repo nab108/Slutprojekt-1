a0aa62a3ae1dbbc56f204ce554c32fde36916a88
Nabil Naqos <zidane.nabil07@gmail.com>
2026-03-18 12:46:03 +0100

    22

---
+++ Slutprojekt1/Boss.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Slutprojekt1;
+
+public class Boss : Enemy
+{
+public int SpecialAttackDamage { get; set; }
+
+public int Phase { get; set; }
+
+public int MaxPhase { get; set; }
+}
