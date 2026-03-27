68711cb2532e7e83805d597a0ace2c287d0d6251
Nabil Naqos <zidane.nabil07@gmail.com>
2026-03-27 13:27:34 +0100

    2026-03-27

--- Slutprojekt1/Boss.cs
+++ Slutprojekt1/Boss.cs
@@ -8,5 +8,12 @@ public int SpecialAttackDamage { get; set; }
 
 public int Phase { get; set; }
 
+public bool IsEnraged { get; set; }
+
+public int SpecialCoolDown { get; set; }
+
+public int CurrentCoolDown { get; set; }
 public int MaxPhase { get; set; }
+
+
 }
--- Slutprojekt1/Fighter.cs
+++ Slutprojekt1/Fighter.cs
@@ -8,14 +8,26 @@ public class Fighter
     public int Health { get; set; }
 
     public int MaxHealth { get; set; }
-
+public string Name { get; set; }
     public int Attack { get; set; }
 
     public int Defense { get; set; }
 
     public int Stamina { get; set; }
 
-    
+    public bool IsAlive { get; set; }
+
+    public Fighter(string name, int maxHealth, int attack, int defense)
+    {
+        Name = name;
+        MaxHealth = maxHealth;
+        Health = maxHealth;
+        Attack = attack;
+        Defense = defense;
+        IsAlive = true;
+    }
+
+
 
 }
 
