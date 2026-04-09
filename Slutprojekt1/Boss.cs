2942764b20568b0904b633b5eabab11de7e13b50
Nabil Naqos <zidane.nabil07@gmail.com>
2026-04-09 21:07:04 +0200

    0409.2

--- Slutprojekt1/Boss.cs
+++ Slutprojekt1/Boss.cs
@@ -15,10 +15,10 @@ public int SpecialCoolDown { get; set; }
 public int CurrentCoolDown { get; set; }
 public int MaxPhase { get; set; }
 
-public void CheckPhase()
-    {
-        if (Health <= MaxHealth / 2 && Phase == 1) //Metod som kollar om bossen ska byta fas
+public void CheckPhase() //Metod som kollar om bossen ska byta fas
         {
+    {
+        if (Health <= MaxHealth / 2 && Phase == 1)  //när bossen får mindre än 50 HP ändras bossens beteende och då kommer följande variabler.
             Phase = 2;
             Attack += 10; //Öka bossens attack med 10.
             IsEnraged = true; //rage mode sätts på
