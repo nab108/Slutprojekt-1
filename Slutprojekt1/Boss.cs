0f39326db41efc5b6c7ba9ab20c8ac6623ed59ba
Nabil Naqos <zidane.nabil07@gmail.com>
2026-04-09 21:04:11 +0200

    0409

--- Slutprojekt1/Boss.cs
+++ Slutprojekt1/Boss.cs
@@ -15,5 +15,13 @@ public int SpecialCoolDown { get; set; }
 public int CurrentCoolDown { get; set; }
 public int MaxPhase { get; set; }
 
-
-}
+public void CheckPhase()
+    {
+        if (Health <= MaxHealth / 2 && Phase == 1) //Metod som kollar om bossen ska byta fas
+        {
+            Phase = 2;
+            Attack += 10; //Öka bossens attack med 10.
+            IsEnraged = true; //rage mode sätts på
+            Console.WriteLine($"{Name} går in i phase 2!");
+        }
+}}
